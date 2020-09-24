31353dd06f9928789d782fea42e82b597affb942
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-24 14:03:02 -0700

    Add new json

--- js/data.js
+++ js/data.js
@@ -1,51 +1,75 @@
 var repos = [
-{
-"name":".scripts",
-"desc":"A collection of small scripts I use on my arch install instead of some other DE utils",
-"url":"https://github.com/JakeRoggenbuck/.scripts",
-},
+	{
+		"name": ".scripts",
+		"desc": "A collection of small scripts I use on my arch install instead of some other DE utils",
+		"url": "https://github.com/JakeRoggenbuck/.scripts"
+	},
 
-{
-"name":"ConfigFiles",
-"desc":"Configs for bspwm, sxhkd, polybar, dunst, nvim, vim, and more ",
-"url":"https://github.com/JakeRoggenbuck/ConfigFiles",
-},
+	{
+		"name": "mongoc",
+		"desc": "A terminal based mongodb viewer",
+		"url": "https://github.com/JakeRoggenbuck/mongoc"
+	},
 
-{
-"name":"impulse",
-"desc":"A simple package manager and build system for *nix systems",
-"url":"https://github.com/JakeRoggenbuck/impulse",
-},
+	{
+		"name": "typeschemalib",
+		"desc": " A yaml like schema that can be used to check dictionaries for correct schema ",
+		"url": "https://github.com/JakeRoggenbuck/typeschemalib"
+	},
 
-{
-"name":"cruz-lang",
-"desc":"Cruz Lang is a simple explicit static typed language.",
-"url":"https://github.com/JakeRoggenbuck/cruz-lang",
-},
+	{
+		"name": "dotfiles",
+		"desc": "",
+		"url": "https://github.com/JakeRoggenbuck/dotfiles"
+	},
 
-{
-"name":"RushNote",
-"desc":"Take quick notes from command line when in a rush.",
-"url":"https://github.com/JakeRoggenbuck/RushNote",
-},
+	{
+		"name": "impulse",
+		"desc": "A simple package manager and build system for *nix systems",
+		"url": "https://github.com/JakeRoggenbuck/impulse"
+	},
 
-{
-"name":"pogscript",
-"desc":"A simple macro language",
-"url":"https://github.com/JakeRoggenbuck/pogscript",
-},
+	{
+		"name": "MongoShortener",
+		"desc": "",
+		"url": "https://github.com/JakeRoggenbuck/MongoShortener"
+	},
 
-{
-"name":"Skyometer",
-"desc":"A website written in google app script and javascript using google sheets as a data collector",
-"url":"https://github.com/JakeRoggenbuck/Skyometer",
-},
+	{
+		"name": "mongo_passkeep",
+		"desc": "",
+		"url": "https://github.com/JakeRoggenbuck/mongo_passkeep"
+	},
 
-{
-"name":"Highlight",
-"desc":"A simple social media website for sharing messages",
-"url":"https://github.com/JakeRoggenbuck/Highlight",
-},
+	{
+		"name": "cruz-lang",
+		"desc": "Cruz Lang is a simple explicit static typed language.",
+		"url": "https://github.com/JakeRoggenbuck/cruz-lang"
+	},
+
+	{
+		"name": "RushNote",
+		"desc": "Take quick notes from command line when in a rush.",
+		"url": "https://github.com/JakeRoggenbuck/RushNote"
+	},
+
+	{
+		"name": "pogscript",
+		"desc": "A simple macro language",
+		"url": "https://github.com/JakeRoggenbuck/pogscript"
+	},
+
+	{
+		"name": "Skyometer",
+		"desc": "A website written in google app script and javascript using google sheets as a data collector",
+		"url": "https://github.com/JakeRoggenbuck/Skyometer"
+	},
+
+	{
+		"name": "Highlight",
+		"desc": "A simple social media website for sharing messages",
+		"url": "https://github.com/JakeRoggenbuck/Highlight"
+	}
 ];
 
 var links = [
