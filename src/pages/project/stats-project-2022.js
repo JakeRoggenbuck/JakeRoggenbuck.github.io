461128f47aa9bbee36cbc01bce7b573dc2cf8529
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-05-24 13:30:45 -0700

    Add basic stats project

---
+++ src/pages/project/stats-project-2022.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Footer from '../../elements/footer';
+
+import {
+  pageStyles,
+  headingStyles,
+  listStyles,
+  listItemStyles,
+  linkStyle,
+  descriptionStyle,
+} from '../../style';
+
+const StatsProject = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Stats Project 2022 - Jake Roggenbuck</title>
+      <h1 style={headingStyles}>Stats Project 2022</h1>
+
+      <Footer />
+    </main>
+  );
+};
+
+export default StatsProject;
