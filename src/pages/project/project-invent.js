583414a7aa4fee99914624c6cba68ad140b77074
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2022-05-31 19:52:41 -0700

    Add more text

--- src/pages/project/project-invent.js
+++ src/pages/project/project-invent.js
@@ -7,7 +7,6 @@ import {
   listStyles,
   listItemStyles,
   linkStyle,
-  descriptionStyle,
 } from '../../style';
 
 const links = [
@@ -16,35 +15,39 @@ const links = [
     url: 'auto-clock-speed',
     link: 'https://github.com/jakeroggenbuck/auto-clock-speed',
     description:
-      'A utility to check stats about your CPU, and auto regulate clock speeds to help with either performance or battery life.',
+      'A utility to check stats about your CPU, and auto regulate clock speeds to help with either performance or battery life. This is a project that I put a lot of time into, more than any other project outside of school, and has three active developers.',
     color: '#fdb549',
   },
   {
     text: 'Local List',
     url: 'local-list',
     link: 'http://thelocallist.org',
-    description: 'Local goods and service discovery system.',
+    description:
+      'Local goods and service discovery system. This was apart of my Econ class and was a project where I got to use things I am very familiar with to build cool software.',
     color: '#fdb549',
   },
   {
     text: 'Statistical Tests Rs',
     url: 'statistical-tests-rs',
     link: 'https://github.com/jakeroggenbuck/statistical-tests-rs',
-    description: 'Statistical Tests for Rust',
+    description:
+      "Statistical Tests for Rust. I noticed that there is not a way to do any statistics other than mean and standard deviation of a population in Rust. The library doesn't even meantion that it is just of a population, because there is standard deviation of a sample as well. I made both, and also made a two sample t test. This is so I can possibly made a tool in the future that helps with stats work, and I will use this library to compile it to wasm and then use it in a Javascript page so that it is fast, accurate, and usable in the browser.",
     color: '#fdb549',
   },
   {
     text: 'Ocean Game Py',
     url: 'ocean-game-py',
     link: 'https://github.com/jakeroggenbuck/ocean-game-py',
-    description: 'Simple ocean game demo in Python for DV Game Jam',
+    description:
+      'Simple ocean game demo in Python for DV Game Jam. This is a demo of how to build a simple game using pygame in Python, and includes slides showing step by step what stuff does.',
     color: '#fdb549',
   },
   {
     text: 'Ocean Game Js',
     url: 'ocean-game-js',
     link: 'https://github.com/jakeroggenbuck/ocean-game-js',
-    description: 'Simple ocean game demo in Javascript for DV Game Jam',
+    description:
+      'Simple ocean game demo in Javascript for DV Game Jam. This includes a slide show that gives step by step instructions of how things work.',
     color: '#fdb549',
   },
 ];
