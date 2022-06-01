e521bc77f1ac7687efd5f8c7b14d66a69fe95ccb
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2022-05-31 17:06:15 -0700

    Add project invent and stats stuff

--- src/pages/project/project-invent.js
+++ src/pages/project/project-invent.js
@@ -13,9 +13,38 @@ import {
 const links = [
   {
     text: 'Auto Clock Speed',
-    url: '#auto-clock-speed',
-	  link: 'https://github.com/jakeroggenbuck/auto-clock-speed',
-    description: 'A utility to check stats about your CPU, and auto regulate clock speeds to help with either performance or battery life.',
+    url: 'auto-clock-speed',
+    link: 'https://github.com/jakeroggenbuck/auto-clock-speed',
+    description:
+      'A utility to check stats about your CPU, and auto regulate clock speeds to help with either performance or battery life.',
+    color: '#fdb549',
+  },
+  {
+    text: 'Local List',
+    url: 'local-list',
+    link: 'http://thelocallist.org',
+    description: 'Local goods and service discovery system.',
+    color: '#fdb549',
+  },
+  {
+    text: 'Statistical Tests Rs',
+    url: 'statistical-tests-rs',
+    link: 'https://github.com/jakeroggenbuck/statistical-tests-rs',
+    description: 'Statistical Tests for Rust',
+    color: '#fdb549',
+  },
+  {
+    text: 'Ocean Game Py',
+    url: 'ocean-game-py',
+    link: 'https://github.com/jakeroggenbuck/ocean-game-py',
+    description: 'Simple ocean game demo in Python for DV Game Jam',
+    color: '#fdb549',
+  },
+  {
+    text: 'Ocean Game Js',
+    url: 'ocean-game-js',
+    link: 'https://github.com/jakeroggenbuck/ocean-game-js',
+    description: 'Simple ocean game demo in Javascript for DV Game Jam',
     color: '#fdb549',
   },
 ];
@@ -26,11 +55,13 @@ const ProjectInvent = () => {
       <title>Project Invent - Jake Roggenbuck</title>
       <h1 style={headingStyles}>Project Invent</h1>
 
+      <h2>Here is a list of portfolio projects for my Project Invent Class.</h2>
+
       <ul style={listStyles}>
         {links.map((link) => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
-              <a style={linkStyle} href={link.url}>
+              <a style={linkStyle} href={'#' + link.url}>
                 {link.text}
               </a>
             </span>
@@ -38,16 +69,18 @@ const ProjectInvent = () => {
         ))}
       </ul>
 
-
-        {links.map((link) => (
-          <div id={link.url} style={{ ...listItemStyles}}>
-            <span>
-				<h2 style={{color: link.color}}>{link.text}</h2>
-				<p>{link.description}</p>
-				<a href={link.link}><p>{link.link}</p></a>
-            </span>
-          </div>
-        ))}
+      {links.map((link) => (
+        <div id={link.url} style={{ ...listItemStyles }}>
+          <span>
+            <h2 style={{ color: link.color }}>{link.text}</h2>
+            <p>{link.description}</p>
+            <a href={link.link}>
+              <p>{link.link}</p>
+            </a>
+          </span>
+          <br />
+        </div>
+      ))}
 
       <Footer />
     </main>
--- src/pages/project/stats-project-2022.js
+++ src/pages/project/stats-project-2022.js
@@ -16,7 +16,12 @@ const StatsProject = () => {
       <title>Stats Project 2022 - Jake Roggenbuck</title>
       <h1 style={headingStyles}>Stats Project 2022</h1>
 
-		<a href="https://github.com/JakeRoggenbuck/stats-project-2022/blob/main/Graphs.ipynb">Graphs.ipynb</a>
+      <a href="https://github.com/JakeRoggenbuck/stats-project-2022/blob/main/Graphs.ipynb">
+        Jupyter Lab for the project - Graphs.ipynb
+      </a>
+
+      <br />
+      <br />
 
       <Footer />
     </main>
